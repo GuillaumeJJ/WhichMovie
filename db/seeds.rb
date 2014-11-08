678dbddbb6baf109c049844541232b8ac3ccc099
Guillaume Jouffre <guillaume.jouffre19@gmail.com>
2014-11-08 16:54:02 +0100

    Sidebars virées

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,113 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Film.create(
+	   titre: "Interstellar",
+	   realisateur: "Christopher Nolan",
+	   acteurs: "Matthew McConaughey, Anne Hathaway, Michael Caine",
+	   BO: "//www.youtube.com/embed/VaOijhK3CRU",
+	   synopsis:"Le film raconte les aventures d’un groupe d’explorateurs qui utilisent une faille récemment découverte dans l’espace-temps afin de repousser les limites humaines et partir à la conquête des distances astronomiques dans un voyage interstellaire." ,
+	   datesortie:"2014-10-5",
+	)
+
+Film.create(
+	titre: "Ninja Turtles",
+	realisateur: "Jonathan Liebesman",
+	acteurs: "Megan Fox, Will Arnett, William Fichtner",
+	BO: "//www.youtube.com/embed/mff5fnnoYmM",
+	synopsis:"New York City a sombré dans les ténèbres depuis que Shredder et son cruel clan des Foot ont la mainmise sur la ville, des forces de police aux politiciens. Alors que l’avenir semble sans espoir, quatre frères en marge de la société sortent des entrailles de la ville pour accomplir leur destin de Tortues Ninja.",
+	datesortie:"2014-9-15",
+	)
+
+Film.create(
+	titre: "La Labirynthe",
+	realisateur: "Wes Ball",
+	acteurs: "Dylan O'Brien, Aml Ameen, Will Poulter",
+	BO: "//www.youtube.com/embed/PL6nMcpeb8I",
+	synopsis:"Quand Thomas reprend connaissance, il est pris au piège avec un groupe d’autres garçons dans un labyrinthe géant dont le plan est modifié chaque nuit. Il n’a plus aucun souvenir du monde extérieur, à part d’étranges rêves à propos d’une mystérieuse organisation appelée W.C.K.D.",
+	datesortie:"2014-9-15",
+	)
+
+Film.create(
+	titre: "Gone Girl",
+	realisateur: "David Fincher",
+	acteurs: "Ben Affleck, Rosamund Pike, Neil Patrick Harris",
+	BO: "//www.youtube.com/embed/ykIaPN3fv-c",
+	synopsis:"A l’occasion de son cinquième anniversaire de mariage, Nick Dunne signale la disparition de sa femme, Amy. Sous la pression de la police et l’affolement des médias, l’image du couple modèle commence à s’effriter. Très vite, les mensonges de Nick et son étrange comportement amènent tout le monde à se poser la même question : a-t-il tué sa femme ?",
+	datesortie:"2014-9-8",
+	)
+
+Film.create(
+	titre: "Mommy",
+	realisateur: "Xavier Dolan",
+	acteurs: "Antoine-Olivier Pilon, Anne Dorval, Suzanne Clément",
+	BO: "//www.youtube.com/embed/8X1fB4ylVuk",
+	synopsis:"Une veuve mono-parentale hérite de la garde de son fils, un adolescent profondément turbulent. Ensemble, ils tentent de joindre les deux bouts, notamment grâce à l’aide inattendue de la mystérieuse voisine d’en face, Kyla.",
+	datesortie:"2014-9-8",
+	)
+
+Film.create(
+	titre: "Le Grimoire d'Arkandias",
+	realisateur: "Alexandre Castagnetti, Julien Simonet",
+	acteurs: "Christian Clavier, Ryan Brodie, Pauline Brisy",
+	BO: "//www.youtube.com/embed/g7SBmicz__o",
+	synopsis:"Dans le village de Ronenval, tout semble normal. Trop normal pour Théo qui ne rêve que d’une chose : échapper à son destin de boloss. Un jour, il déniche à la bibliothèque un livre de magie qui contient les secrets de fabrication d’une bague d’invisibilité.",
+	datesortie:"2014-9-22",
+	)
+
+Film.create(
+	titre: "Magic in the Moonlight",
+	realisateur: "Woody Allen",
+	acteurs: "Colin Firth, Emma Stone, Eileen Atkins",
+	BO: "//www.youtube.com/embed/YKRfaB9OJiI",
+	synopsis:"Le prestidigitateur chinois Wei Ling Soo est le plus célèbre magicien de son époque, mais rares sont ceux à savoir qu’il s’agit en réalité du nom de scène de Stanley Crawford : cet Anglais arrogant et grognon ne supporte pas les soi-disant médiums qui prétendent prédire l’avenir.",
+	datesortie:"2014-9-22",
+	)
+
+Film.create(
+	titre: "Fury",
+	realisateur: "David Ayer",
+	acteurs: "Brad Pitt, Shia LaBeouf, Logan Lerman",
+	BO: "//www.youtube.com/embed/q94n3eWOWXM",
+	synopsis:"Avril 1945. Les Alliés mènent leur ultime offensive en Europe. À bord d’un tank Sherman, le sergent Wardaddy et ses quatre hommes s’engagent dans une mission à très haut risque bien au-delà des lignes ennemies.",
+	datesortie:"2014-9-22",
+	)
+
+Film.create(
+	titre: "On a marché sur Bangkok",
+	realisateur: "Olivier Baroux",
+	acteurs: "Kad Merad, Alice Taglioni, Peter Coyote",
+	BO: "//www.youtube.com/embed/WM72m0bESq4",
+	synopsis:"Serge Renart, un journaliste TV devenu has-been et Natacha Bison, une reporter de guerre écartée du métier parce que trop dangereuse pour ses collègues, se retrouvent obligés d’enquêter ensemble sur une affaire qui les mènera en Thaïlande...",
+	datesortie:"2014-9-22",
+	)
+
+Film.create(
+	titre: "La Légende de Manolo",
+	realisateur: "Jorge R. Gutierrez",
+	acteurs: "Diego Luna, Zoe Saldana, Channing Tatum",
+	BO: "//www.youtube.com/embed/oZxd8_W8F0Q",
+	synopsis:"Le jeune Manolo est tiraillé entre les attentes de sa famille et ce vers quoi son coeur le porte. Avant de choisir de s'engager sur une voie, il embarque pour une incroyable aventure, qui le conduit dans trois mondes fantastiques, où il lui faudra affronter ses plus grandes peurs.",
+	datesortie:"2014-9-22",
+	)
+
+Film.create(
+	titre: "Les Boxtrolls",
+	realisateur: "Graham Annable, Anthony Stacchi",
+	acteurs: "Isaac Hempstead-Wright, Elle Fanning, Ben Kingsley",
+	BO: "//www.youtube.com/embed/CSomVLvah34",
+	synopsis:"Les Boxtrolls est une fable qui se déroule à Cheesebridge, une ville huppée de l’époque victorienne, dont la principale préoccupation est le luxe, la distinction et la crème des fromages les plus puants. Sous le charme de ses rues pavées, se cachent les Boxtrolls...",
+	datesortie:"2014-9-15",
+	)
+
+Film.create(
+	titre: "Une nouvelle amie",
+	realisateur: "Francois Ozon",
+	acteurs: "Romain Duris, Anaïs Demoustier, Raphaël Personnaz",
+	BO: "//www.youtube.com/embed/lxNUmOe-dhY",
+	synopsis:"À la suite du décès de sa meilleure amie, Claire fait une profonde dépression, mais une découverte surprenante au sujet du mari de son amie va lui redonner goût à la vie.",
+	datesortie:"2014-10-5",
+	)
 
 
 Film.create(
@@ -116,6 +223,17 @@ Film.create(
 	)
 
 Film.create(
+<<<<<<< Updated upstream
+=======
+	titre: "Samba",
+	realisateur: "Eric Toledano, Olivier Nakache",
+	acteurs: "Omar Sy, Charlotte Gainsbourg, Tahar Rahim",
+	BO: "//www.youtube.com/embed/-FsspoYgclE",
+	synopsis:"Samba, sénégalais en France depuis 10 ans, collectionne les petits boulots ; Alice est une cadre supérieure épuisée par un burn out. Lui essaye par tous les moyens d'obtenir ses papiers, alors qu'elle tente de se reconstruire par le bénévolat dans une association." ,
+	datesortie:"2014-10-15",
+	)
+Film.create(
+>>>>>>> Stashed changes
 	titre: "The Giver",
 	realisateur: "Philippe Noyce",
 	acteurs: "Jeff Bridges, Meryl Streep, Brenton Thwaites",
