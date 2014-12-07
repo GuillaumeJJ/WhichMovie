991ea1756f14f66331f197e3fcbdd7731cc4c2bf
Guillaume Jouffre <guillaume.jouffre19@gmail.com>
2014-12-07 14:18:58 +0100

    little fix

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,5 @@
 class Film < ActiveRecord::Base
 	attr_accessible :titre, :realisateur, :acteurs, :BO, :synopsis, :datesortie,:affiche
-	has_many :seances, :likes
+	has_many :seances
+	has_many :likes
 end
