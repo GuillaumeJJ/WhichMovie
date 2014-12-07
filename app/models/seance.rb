485c3aba756d60187cb7fdbb19cadb1e4ddc7a75
Guillaume Jouffre <guillaume.jouffre19@gmail.com>
2014-12-08 00:06:24 +0100

    fix seances

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -2,6 +2,4 @@ class Seance < ActiveRecord::Base
 	attr_accessible :heure, :film_id, :cinema_id
 	belongs_to :cinema
 	belongs_to :film
-	attr_accessible :heure, :cinema_id, :film_id
-	belongs_to :cinema, :film
 end
