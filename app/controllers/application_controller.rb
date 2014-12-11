c3afd3d5e46403d42df2a160cc66c553bb9f9866
Guillaume Jouffre <guillaume.jouffre19@gmail.com>
2014-12-11 03:13:52 +0100

    ciné de méde

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    before_filter :allow_ajax_request_from_other_domains
+
+  def allow_ajax_request_from_other_domains
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+  end
 end
